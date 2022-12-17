bb53a5a58ddf2df00ed4b86cd1bd188b232af916
Andre <AndreLeos95@gmail.com>
2022-12-16 19:03:44 -0800

    clean up material page

--- material.java
+++ material.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class material {
 
 // Class data members
-// generated
-private static int uniqueNoCounter  = 1000 ;
-private static int locNoCounter     = 100  ;
+// generated location numbers and Unique Counter
+private static int uniqueNoCounter  	= 1000 ;
+private static int locMagNoCounter     	= 100  ;
+private static int locBookNoCounter     = 100  ;
+private static int locDVDNoCounter     	= 100  ;
 //functionality
 public int         inputValidCheck  = 0    ;
 public int 		   r18Check  		= 0    ;
@@ -33,7 +35,7 @@ Scanner scan = new Scanner(System.in);
 // Display message for taking scan
 public material () throws Exception {
 	while (inputValidCheck == 0) {
-		System.out.println("What type of material are you adding today? Please type: B for book, D for DVD, or M for Magzine!");
+		System.out.println("What type of material are you adding today? Please type: B for book, D for DVD, or M for Magazine!");
 		this.locName = scan.next().charAt(0);
 		System.out.println(scan.nextLine());
 			if (locName == 66 || locName == 68 ||locName == 77 ||locName == 98 || locName == 100 || locName == 109) {
@@ -82,21 +84,34 @@ public material () throws Exception {
 			System.out.println("Please enter a valid input!");
 			}
 		    this.uniqueNo = ++uniqueNoCounter;
-		    this.locNo 	  = ++locNoCounter;
 			}
+	if (locName == 66 ||locName == 98) {
+			this.loanTime = 7;
+			System.out.println("This item will have " + loanTime + " days to return it.");
+		    this.locNo 	  = ++locBookNoCounter;
+		} else if (locName == 77 ||locName == 109) {
+			this.loanTime = 3;
+			this.locNo 	  = ++locMagNoCounter;
+			System.out.println("This item will have " + loanTime + " days to return it.");
+		} else if(locName == 68 ||locName == 100) {
+			this.loanTime = 2;
+			this.locNo 	  = ++locDVDNoCounter;
+			System.out.println("This item will have " + loanTime + " days to return it.");	
+		}
+	
 	//debug
-	System.out.println(title);                                              //
-	System.out.println("Location Zone:" + locName);                         //
-	System.out.println("Author Name:" + authorName);                        //
-	System.out.println("ISBN:" + isbn);                                     //
-	System.out.println("Length:" + length);                                 //
-	System.out.println("Released:" + released);
-	System.out.println("issueNumber:" + issueNumber);
-	System.out.println("R18:" + r18);                                       //
-	System.out.println("Location Number:" + locNoCounter);
-	System.out.println("Unique Identifier for item:" + uniqueNo);           //
-	System.out.println("Unique Identifier Total Count:" + uniqueNoCounter); //
-	System.out.println("Location Identifier for item:" + locName + "." + locNo);
-	System.out.println("Location Identifier Total Count:" + locNoCounter);
+	//System.out.println(title);                                              //
+	//System.out.println("Location Zone:" + locName);                         //
+	//System.out.println("Author Name:" + authorName);                        //
+	//System.out.println("ISBN:" + isbn);                                     //
+	//System.out.println("Length:" + length);                                 //
+	//System.out.println("Released:" + released);
+	//System.out.println("issueNumber:" + issueNumber);
+	//System.out.println("R18:" + r18);                                       //
+	//System.out.println("Location Number:" + locNoCounter);
+	//System.out.println("Unique Identifier for item:" + uniqueNo);           //
+	//System.out.println("Unique Identifier Total Count:" + uniqueNoCounter); //
+	//System.out.println("Location Identifier for item:" + locName + "." + locNo);
+	//System.out.println("Location Identifier Total Count:" + locNoCounter);
 	}
 	}
